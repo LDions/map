91cfba61ed68d1dbf2265e6905a41740c00862f2
hanzongyan <hanzongyan0106@163.com>
2022-03-22 18:50:07 +0800

    code review

--- src/main/java/com/ruowei/domain/Group.java
+++ src/main/java/com/ruowei/domain/Group.java
@@ -40,13 +40,6 @@ public class Group {
     private String groupName;
 
     /**
-     * 集团编码
-     */
-    @ApiModelProperty(value = "集团编码")
-    @Column(name = "group_code")
-    private String groupCode;
-
-    /**
      * 集团位置
      */
     @ApiModelProperty(value = "集团位置")
@@ -126,14 +119,6 @@ public class Group {
     }
     public void setGroupContactName(String groupContactName) {
         this.groupContactName = groupContactName;
-    }
-
-    public String getGroupCode() {
-        return groupCode;
-    }
-
-    public void setGroupCode(String groupCode) {
-        this.groupCode = groupCode;
     }
 
     public String getGroupContactPhone() {
